adb7bbff783e981d6b652ba02b2241b94d02b80d
pawel-0904 <p0904@mail.ru>
2020-09-23 14:17:43 +0300

    #PGRABKO First_commit

---
+++ src/test/java/first_project/example/AppTest.java
@@ -0,0 +1,20 @@
+package first_project.example;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
